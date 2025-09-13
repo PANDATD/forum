c1a727c25792c99291c458f926c33651613d281f
MR. TEJAS DIXIT <tejasdixit17@gmail.com>
2025-09-13 13:55:53 +0000

    updated __init.py included config for the same

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os 
+
+db = SQLAlchemy()
+
+
 
 def create_app():
     app = Flask(__name__)
 
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASE_DIR, "..", "forum.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+    db.init_app(app)
+    
     # Import routes
     from .routes import main
     app.register_blueprint(main)
-
+    
     return app
